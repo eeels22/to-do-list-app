3024b8fc7e0af842974d626abf1618d258d81b33
En-Chi Liu <enchi.liu@gmail.com>
2021-03-06 14:02:28 +0100

    Updated FileHandler documentation and field

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -9,18 +9,29 @@ import java.io.*;
 
 public class FileHandler {
 
+    // The task list on which input and output operations are performed
     private TaskList taskList;
+    // The path in the root directory where the file is located
     private String path = "src/main/resources/";
+    // File name and extension
+    private String fileName = "taskList.txt";
 
     /**
-     * Saves the task list to a file.
+     * Constructor for the file handler.
      *
-     * @throws IOException
+     * @param taskList The task list to save and load.
+     */
+    public FileHandler(TaskList taskList) {
+        this.taskList = taskList;
+    }
+
+    /**
+     * Saves the task list to a file. Prints an error message if an exception is caught.
      */
     public void saveTaskListToFile(TaskList taskList) {
 
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(path + "taskList.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream(path + fileName);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
             objectOutputStream.writeObject(taskList);
@@ -34,14 +45,14 @@ public class FileHandler {
     }
 
     /**
-     * Loads a task list from a file.
+     * Loads a task list from a file. Prints an error message if an exception is caught.
      *
      * @throws IOException
      */
     public TaskList loadTaskListFromFile() {
 
         try {
-            FileInputStream fileInputStream = new FileInputStream(path + "taskList.txt");
+            FileInputStream fileInputStream = new FileInputStream(path + fileName);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 
             taskList = (TaskList) objectInputStream.readObject();
@@ -51,7 +62,9 @@ public class FileHandler {
 
         } catch (IOException | ClassNotFoundException exception) {
             System.out.println("Oops, there's a problem with loading the file: " + exception);
+            // Could the message here be that the task list is empty?
         }
         return taskList;
     }
 }
+
