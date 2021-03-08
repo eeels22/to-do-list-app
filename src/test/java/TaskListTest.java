8e5708442ebc0611b01ccaa24bd4344ffe6ff4b6
En-Chi Liu <enchi.liu@gmail.com>
2021-03-08 20:23:06 +0100

    Created TaskListTest class

---
+++ src/test/java/TaskListTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskListTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createTask() {
+        TaskList taskList1 = new TaskList();
+        taskList1.createTask();
+
+    }
+
+    @Test
+    void testToString() {
+        TaskList taskList1 = new TaskList();
+        taskList1.getTasks().add(new Task("Eat lunch", LocalDate.of(2021, 11, 20), "Stay alive"));
+        taskList1.getTasks().add(new Task("Read about Streams", LocalDate.of(2021, 5, 12), "Learn Java"));
+        taskList1.toString();
+    }
+}
